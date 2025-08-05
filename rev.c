b7a616ed1221a3271ab0e882127b6eee03f66863
Rajasree14 <k.rajasree14@gmail.com>
2025-08-05 10:29:28 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int reverse(int arr[],int n){
+    int *ptr1,*ptr2;
+    ptr1=arr;
+    ptr2=&arr[n-1];
+    while(ptr1<ptr2){
+        int temp=*ptr1;
+        *ptr1=*ptr2;
+        *ptr2=temp;
+        ptr1++;
+        ptr2--;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    reverse(arr,n);
+    for(int i=0;i<n;i++)
+    printf("%d ",arr[i]);
+}
